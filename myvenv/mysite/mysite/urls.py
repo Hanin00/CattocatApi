8f3530aca2d8c134590cfb3b84c5ce50d7e09320
DESKTOP-KP2SU33\haeun <hhaeun5419@gmail.com>
2021-10-09 13:23:22 +0900

    Profile Page

--- myvenv/mysite/mysite/urls.py
+++ myvenv/mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cats/', include('cats.urls')),
+    path('profile/', include('accounts.urls')),
 ]
